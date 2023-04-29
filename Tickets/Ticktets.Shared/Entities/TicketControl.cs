e50ea78449f7e98b90516241bf028d8a6651baae
Yeison Arenas <yeisonarenas177020@correo.itm.edu.co>
2023-04-28 19:16:04 -0500

    initial commit

---
+++ Tickets/Ticktets.Shared/Entities/TicketControl.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ticktets.Shared.Entities
+{
+    public class TicketControl
+    {
+        public int Id { get; set; }
+
+        public string? TicketID { get; set; }
+
+        public DateTime? UseDate { get; set; }
+
+        public bool IsUsed { get; set; }
+
+        public string? Tribune { get; set; }
+
+    }
+}
